9344ba7a5306883a58dce2a21f0c549e1465efea
yy158775 <1584616775>
2021-02-09 21:33:00 +0800

    first

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int k = 3;
+	for(int i = 1;i < 2;i++)
+	{
+		int temp = 0;
+		
+		for(int j = 1;j <= 1;j++)
+		{
+			//cout<<temp;
+			
+			while(k--)
+			{
+				cout<<temp;
+			}
+		}
+	}
+ } 
